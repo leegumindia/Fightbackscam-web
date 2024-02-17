fe09a6aa8c8f1059d0156c4e0a00b993d5158f59
petulla <petulla@mitrarobot.com>
2024-02-17 18:48:56 +0530

    color theme change testing

--- scaminformative-web/src/components/HowHelp.js
+++ scaminformative-web/src/components/HowHelp.js
@@ -4,6 +4,9 @@ import'../components/HowHelp.css';
 import image1 from "../images/registerComplaint.png";
 import image2 from "../images/unity.png";
 import image3 from "../images/justice.png";
+import image4 from "../images/complain.png";
+import image5 from "../images/unity1.png";
+import image6 from "../images/justice1.png";
 
 function HowHelp() {
    return (
@@ -126,6 +129,121 @@ function HowHelp() {
       </div>
     </div>
   </section>
+  <section  id="header" className="d-flex align-items-center ">
+    
+    <div className="container-fluid nav_bg"style={{overflowX:'hidden'}}>
+      <div className="row">
+        <div className="col-10 mx-auto">
+          <div className="row" >
+          <div className="col-6 mt-3 header-img"data-aos="zoom-in">
+              <img
+                src={image4}
+                className="img-fluid sm:w-screen lg:w-screen mt-5"
+                alt="home-img"
+                style={{height:'70%'}}
+              />
+            </div>
+            <div className="col-6  d-flex justify-content-center flex-column home-text">
+            <h2  style={{color:'#2D5767'}}>
+                <strong>
+                Register your Scam      
+                </strong>
+              </h2>
+            
+              <p className="my-3 content" style={{color:'black'}}>
+              Have you or someone you care about been <span style={{fontWeight:'bold'}}>scammed online?</span><br></br> 
+              Tell us your story and help others avoid the same <span style={{fontWeight:'bold'}}>fraud</span>.<br></br> 
+              We're here to listen and learn, so the more details you share, the better we can fight!  <br></br>
+              </p>
+              
+              <div className="mt-4 pb-5 app-button">
+               <a href="" target="_blank">
+               <button className="btn btn-default  mr-2" style={{backgroundColor:'#2D5767',color:'white'}}>Report your Scam</button>
+                </a>
+             </div>
+            </div>
+            
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section  id="header" className="d-flex align-items-center ">
+
+    <div className="container-fluid nav_bg"style={{overflowX:'hidden'}}>
+      <div className="row">
+        <div className="col-10 mx-auto">
+          <div className="row" >
+          <div className="col-7  d-flex justify-content-center flex-column home-text">
+            <h1  style={{color:'#2D5767'}}>
+                <strong>
+                Collecting proofs nation wide    
+                </strong>
+              </h1>
+            
+              <p className="my-3 content" style={{color:'#2D5767'}}>
+              Our team is actively <span style={{fontWeight:'bold'}}>collecting evidence from across India</span> to build strong cases<br></br>
+              against scammers. By submitting your proofs, you contribute to our collective <br></br>
+              effort to combat fraud
+              </p>
+              
+              <div className="mt-4 pb-5 app-button">
+               
+             </div>
+            </div>
+          <div className="col-5 mt-3 header-img"data-aos="zoom-in">
+              <img
+                src={image5}
+                className="img-fluid sm:w-screen lg:w-screen mt-5"
+                alt="home-img"
+                // style={{height:'80%'}}
+              />
+            </div>
+            
+            
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section  id="header" className="d-flex align-items-center ">
+
+<div className="container-fluid nav_bg"style={{overflowX:'hidden'}}>
+  <div className="row">
+    <div className="col-10 mx-auto">
+      <div className="row" >
+      <div className="col-6 mt-3 header-img"data-aos="zoom-in">
+          <img
+            src={image6}
+            className="img-fluid sm:w-screen lg:w-screen mt-5"
+            alt="home-img"
+            style={{height:'70%'}}
+          />
+        </div>
+        <div className="col-6  d-flex justify-content-center flex-column home-text">
+        <h1  style={{color:'#2D5767'}}>
+            <strong>
+            Fight against scams    
+            </strong>
+          </h1>
+        
+          <p className="my-3 content" style={{color:'black'}}>
+          <span style={{fontWeight:'bold'}}>We won't stop until justice is served</span>. <br>
+          </br>Once we have enough evidence, we'll bring your case directly to the police or relevant authorities. 
+          We'll advocate for you every step of the way, ensuring your complaint is registered and swift action is taken
+          </p>
+          
+       
+        </div>
+        
+        
+      </div>
+    </div>
+  </div>
+</div>
+</section>
     </>
    );
 }
