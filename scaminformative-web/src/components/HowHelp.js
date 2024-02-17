4e6087fbf2ce658be6903b52bcef1071138c5852
petulla <petulla@mitrarobot.com>
2024-02-17 22:27:58 +0530

    responsive layout

--- scaminformative-web/src/components/HowHelp.js
+++ scaminformative-web/src/components/HowHelp.js
@@ -20,16 +20,8 @@ function HowHelp() {
           <div className="row">
             <div className="col-10 mx-auto">
               <div className="row" >
-              <div className="col-6 mt-3 header-img"data-aos="zoom-in">
-                  <img
-                    src={image1}
-                    className="img-fluid sm:w-screen lg:w-screen mt-5"
-                    alt="home-img"
-                    style={{height:'70%'}}
-                  />
-                </div>
-                <div className="col-6  d-flex justify-content-center flex-column home-text">
-                <h2  style={{color:'#EF7820'}}>
+              <div className="col-md-7  pt-lg-0 orer-2 order-lg-1 d-flex justify-content-center flex-column home-text1">
+                <h2 className="sub-text" style={{color:'#EF7820'}}>
                     <strong>
                     Register your Scam      
                     </strong>
@@ -41,12 +33,21 @@ function HowHelp() {
                   We're here to listen and learn, so the more details you share, the better we can fight!  <br></br>
                   </p>
                   
-                  <div className="mt-4 pb-5 app-button">
+                  <div className="mt-4 app-button">
                    <a href="" target="_blank">
                    <button className="btn btn-default  mr-2" style={{backgroundColor:'#EF7820',color:'white'}}>Report your Scam</button>
                     </a>
                  </div>
                 </div>
+              <div className="col-lg-5 col-sm-app col-md-5 order-1 order-lg-2 header-img1"data-aos="zoom-in">
+                  <img
+                    src={image1}
+                    className="img-fluid sm:w-screen lg:w-screen mt-5"
+                    alt="home-img"
+                   
+                  />
+                </div>
+               
                 
                 
               </div>
@@ -60,7 +61,7 @@ function HowHelp() {
           <div className="row">
             <div className="col-10 mx-auto">
               <div className="row" >
-              <div className="col-7  d-flex justify-content-center flex-column home-text">
+              <div className="col-md-7 pt-5 pt-lg-0 orer-2 order-lg-1 d-flex justify-content-center flex-column home-text">
                 <h1  style={{color:'#EF7820'}}>
                     <strong>
                     Collecting proofs nation wide    
@@ -73,14 +74,12 @@ function HowHelp() {
                   effort to combat fraud
                   </p>
                   
-                  <div className="mt-4 pb-5 app-button">
-                   
-                 </div>
+                 
                 </div>
-              <div className="col-5 mt-3 header-img"data-aos="zoom-in">
+              <div className=" col-lg-5 col-sm-app col-md-5 order-1 order-lg-2 header-img"data-aos="zoom-in">
                   <img
                     src={image2}
-                    className="img-fluid sm:w-screen lg:w-screen mt-5"
+                    className="img-fluid sm:w-screen lg:w-screen"
                     alt="home-img"
                     // style={{height:'80%'}}
                   />
@@ -99,105 +98,25 @@ function HowHelp() {
       <div className="row">
         <div className="col-10 mx-auto">
           <div className="row" >
-          <div className="col-6 mt-3 header-img"data-aos="zoom-in">
-              <img
-                src={image3}
-                className="img-fluid sm:w-screen lg:w-screen mt-5"
-                alt="home-img"
-                style={{height:'70%'}}
-              />
-            </div>
-            <div className="col-6  d-flex justify-content-center flex-column home-text">
+          <div className="col-md-7 pt-5 pt-lg-0 orer-2 order-lg-1 d-flex justify-content-center flex-column home-text">
             <h1  style={{color:'#EF7820'}}>
                 <strong>
-                Fight against scams    
+                Fight against scams 
                 </strong>
               </h1>
             
               <p className="my-3 content" style={{color:'black'}}>
-              <span style={{fontWeight:'bold'}}>We won't stop until justice is served</span>. <br>
-              </br>Once we have enough evidence, we'll bring your case directly to the police or relevant authorities. 
-              We'll advocate for you every step of the way, ensuring your complaint is registered and swift action is taken
+              <span style={{fontWeight:'bold'}}>We won't stop until justice is served</span>. Once we have enough evidence, we'll bring your case directly to the police or relevant authorities. We'll advocate for you every step of the way, ensuring your complaint is registered and swift action is taken
               </p>
               
-           
+             
             </div>
-            
-            
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <section  id="header" className="d-flex align-items-center ">
-    
-    <div className="container-fluid nav_bg"style={{overflowX:'hidden'}}>
-      <div className="row">
-        <div className="col-10 mx-auto">
-          <div className="row" >
-          <div className="col-6 mt-3 header-img"data-aos="zoom-in">
+          <div className=" col-lg-5 col-sm-app col-md-5 order-1 order-lg-2 header-img2"data-aos="zoom-in">
               <img
-                src={image4}
-                className="img-fluid sm:w-screen lg:w-screen mt-5"
+                src={image3}
+                className="img-fluid sm:w-screen lg:w-screen"
                 alt="home-img"
-                style={{height:'70%'}}
-              />
-            </div>
-            <div className="col-6  d-flex justify-content-center flex-column home-text">
-            <h2  style={{color:'#2D5767'}}>
-                <strong>
-                Register your Scam      
-                </strong>
-              </h2>
-            
-              <p className="my-3 content" style={{color:'black'}}>
-              Have you or someone you care about been <span style={{fontWeight:'bold'}}>scammed online?</span><br></br> 
-              Tell us your story and help others avoid the same <span style={{fontWeight:'bold'}}>fraud</span>.<br></br> 
-              We're here to listen and learn, so the more details you share, the better we can fight!  <br></br>
-              </p>
-              
-              <div className="mt-4 pb-5 app-button">
-               <a href="" target="_blank">
-               <button className="btn btn-default  mr-2" style={{backgroundColor:'#2D5767',color:'white'}}>Report your Scam</button>
-                </a>
-             </div>
-            </div>
-            
-            
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <section  id="header" className="d-flex align-items-center ">
-
-    <div className="container-fluid nav_bg"style={{overflowX:'hidden'}}>
-      <div className="row">
-        <div className="col-10 mx-auto">
-          <div className="row" >
-          <div className="col-7  d-flex justify-content-center flex-column home-text">
-            <h1  style={{color:'#2D5767'}}>
-                <strong>
-                Collecting proofs nation wide    
-                </strong>
-              </h1>
-            
-              <p className="my-3 content" style={{color:'#2D5767'}}>
-              Our team is actively <span style={{fontWeight:'bold'}}>collecting evidence from across India</span> to build strong cases<br></br>
-              against scammers. By submitting your proofs, you contribute to our collective <br></br>
-              effort to combat fraud
-              </p>
-              
-              <div className="mt-4 pb-5 app-button">
-               
-             </div>
-            </div>
-          <div className="col-5 mt-3 header-img"data-aos="zoom-in">
-              <img
-                src={image5}
-                className="img-fluid sm:w-screen lg:w-screen mt-5"
-                alt="home-img"
-                // style={{height:'80%'}}
+                
               />
             </div>
             
@@ -208,42 +127,10 @@ function HowHelp() {
       </div>
     </div>
   </section>
-  <section  id="header" className="d-flex align-items-center ">
 
-<div className="container-fluid nav_bg"style={{overflowX:'hidden'}}>
-  <div className="row">
-    <div className="col-10 mx-auto">
-      <div className="row" >
-      <div className="col-6 mt-3 header-img"data-aos="zoom-in">
-          <img
-            src={image6}
-            className="img-fluid sm:w-screen lg:w-screen mt-5"
-            alt="home-img"
-            style={{height:'70%'}}
-          />
-        </div>
-        <div className="col-6  d-flex justify-content-center flex-column home-text">
-        <h1  style={{color:'#2D5767'}}>
-            <strong>
-            Fight against scams    
-            </strong>
-          </h1>
-        
-          <p className="my-3 content" style={{color:'black'}}>
-          <span style={{fontWeight:'bold'}}>We won't stop until justice is served</span>. <br>
-          </br>Once we have enough evidence, we'll bring your case directly to the police or relevant authorities. 
-          We'll advocate for you every step of the way, ensuring your complaint is registered and swift action is taken
-          </p>
-          
-       
-        </div>
-        
-        
-      </div>
-    </div>
-  </div>
-</div>
-</section>
+
+
+ 
     </>
    );
 }
