924936e0d258d13e969bebdf3ad2e3185fd935d3
Akash <akash@MacBook-Pro.local>
2024-02-17 23:46:44 +0530

    putting fist in place of i in #fightbackscams

--- scaminformative-web/src/components/Home.js
+++ scaminformative-web/src/components/Home.js
@@ -1,43 +1,66 @@
 import React from "react";
 
-import'../components/Home.css';
+import "../components/Home.css";
 import image from "../images/homepage_graphic.png";
+import fist_navy from "../images/fist_navy.png";
 function App() {
   return (
-   <>
-   <section  id="header" className="d-flex align-items-center ">
-        
-        <div className="container-fluid nav_bg"style={{overflowX:'hidden'}}>
+    <>
+      <section id="header" className="d-flex align-items-center ">
+        <div className="container-fluid nav_bg" style={{ overflowX: "hidden" }}>
           <div className="row">
             <div className="col-10 mx-auto">
-              <div className="row" >
-        
-                <div data-aos="slide-up" className="col-md-7 pt-5  d-flex justify-content-center flex-column home">
-                  
-                  <h1  style={{color:'black'}}>
-                    <strong>
-                     #FightBackScams         
-                    </strong>
-                  </h1>
+              <div className="row">
+                <div
+                  data-aos="slide-up"
+                  className="col-md-7 pt-5  d-flex justify-content-center flex-column home"
+                >
+                  <div className="col-md-7 pt-5  d-flex justify-content-center flex-row">
+                    <h1 style={{ color: "black" }}>
+                      <strong>#F</strong>
+                    </h1>
+                    <img
+                      src={fist_navy}
+                      className="img-fluid sm:w-screen lg:w-screen"
+                      alt="i"
+                    />
+                    <h1 style={{ color: "black" }}>
+                      <strong>ghtBackScams</strong>
+                    </h1>
+                  </div>
                   <br></br>
-                  {/* Grey colour used previously here was: 595656 */}
-                  <h2 className="text-content" style={{ color:'black'}}>
-                 
-                     Take Action Against Online Fraud: We're Here to Help! 
+                  {/* #211b1b colour used previously here was: 595656 */}
+                  <h2 className="text-content" style={{ color: "black" }}>
+                    Take Action Against Online Fraud: We're Here to Help!
                   </h2>
-                  <h2 className="my-3 content" style={{color:'black'}}>
-                  Have you been a victim of <span style={{fontWeight:'bold'}}>online fraud?</span> Whether it's a <span style={{fontWeight:'bold'}}>phishing scam, identity theft</span> experiencing online fraud can be stressful and overwhelming. You're not alone.  <br></br>
+                  <h2 className="my-3 content" style={{ color: "black" }}>
+                    Have you been a victim of{" "}
+                    <span style={{ fontWeight: "bold" }}>online fraud?</span>{" "}
+                    Whether it's a{" "}
+                    <span style={{ fontWeight: "bold" }}>
+                      phishing scam, identity theft
+                    </span>{" "}
+                    experiencing online fraud can be stressful and overwhelming.
+                    You're not alone. <br></br>
                   </h2>
-                  <h3 className=" sub-content"style={{ color:'#488E1B'}}>
-                  Don't hesitate to reach out!
+                  <h3 className=" sub-content" style={{ color: "#488E1B" }}>
+                    Don't hesitate to reach out!
                   </h3>
                   <div className="mt-4 pb-5 app-button">
-                   <a href="" target="_blank">
-                   <button className="btn btn-default  mr-2" style={{backgroundColor:'#EF7820',color:'white'}}>Report your Scam</button>
+                    <a href="" target="_blank">
+                      <button
+                        className="btn btn-default  mr-2"
+                        style={{ backgroundColor: "#EF7820", color: "white" }}
+                      >
+                        Report your Scam
+                      </button>
                     </a>
-                 </div>
+                  </div>
                 </div>
-                <div className="col-lg-5 col-sm-app col-md-5 order-1 order-lg-2 header-img"data-aos="zoom-in">
+                <div
+                  className="col-lg-5 col-sm-app col-md-5 order-1 order-lg-2 header-img"
+                  data-aos="zoom-in"
+                >
                   <img
                     src={image}
                     className="img-fluid sm:w-screen lg:w-screen mt-5"
@@ -50,8 +73,7 @@ function App() {
           </div>
         </div>
       </section>
-
-   </>
+    </>
   );
 }
 
