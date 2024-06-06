24d575de150edec57aa5c528a88e65141491c080
Akash <akash@MacBook-Pro.local>
2024-06-06 20:43:23 +0530

    Midway adding firebase logic

--- scaminformative-web/src/App.js
+++ scaminformative-web/src/App.js
@@ -1,24 +1,41 @@
+import Home from "./components/Home.js";
+import Navbar from "./components/Navbar.js";
+import HowHelp from "./components/HowHelp.js";
+import HowHelp2 from "./components/HowHelp2.js";
+import PhoneVerification from "./components/Buttontrial.js";
+import TypesOfScams from "./components/TypesOfScams.js";
+import LearningCorner from "./components/LearningCorner.js";
+import { db } from "./config/firebaseConfig.js";
 
-import Home from './components/Home.js'
-import Navbar from './components/Navbar.js';
-import HowHelp from './components/HowHelp.js';
-import HowHelp2 from './components/HowHelp2.js';
-import PhoneVerification from './components/Buttontrial.js';
-import TypesOfScams from './components/TypesOfScams.js';
-import LearningCorner from './components/LearningCorner.js';
-
-function App() {
+export default function App() {
+  testFirebase();
   return (
-   <>
-   <Navbar/>
-   <Home/> 
-   <HowHelp2/>
-   <TypesOfScams/>
-   <LearningCorner/>
-
-
-   </>
+    <>
+      <Navbar />
+      <Home />
+      <HowHelp2 />
+      <TypesOfScams />
+      <LearningCorner />
+    </>
   );
 }
 
-export default App;
+// export default App;
+
+function testFirebase() {
+  console.log("AKASH RUN");
+
+  db.collection("cities")
+    .doc("LA")
+    .set({
+      name: "Los Angeles",
+      state: "CA",
+      country: "USA",
+    })
+    .then(() => {
+      console.log("Document successfully written!");
+    })
+    .catch((error) => {
+      console.error("Error writing document: ", error);
+    });
+}
---
+++ scaminformative-web/src/config/firebaseConfig.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// import { getAnalytics } from "firebase/analytics";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAcS9qT-M45i7cin682B9IkfbYW2JO0JBQ",
+//   authDomain: "orderandpickup-731bd.firebaseapp.com",
+//   databaseURL: "https://orderandpickup-731bd-default-rtdb.firebaseio.com",
+//   projectId: "orderandpickup-731bd",
+//   storageBucket: "orderandpickup-731bd.appspot.com",
+//   messagingSenderId: "1098866334933",
+//   appId: "1:1098866334933:web:1893396a7d45158c81d65c",
+//   measurementId: "G-HGE4DR3ELZ"
+// };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDmbDSAe5h2OGMJXbP5w8-NealA81uusus",
+  authDomain: "twitter-b7e65.firebaseapp.com",
+  projectId: "twitter-b7e65",
+  storageBucket: "twitter-b7e65.appspot.com",
+  messagingSenderId: "678357277801",
+  appId: "1:678357277801:web:8b3c79fb898a30b70256f8",
+  measurementId: "G-KQ718E561P",
+};
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+export const db = getFirestore(app);
